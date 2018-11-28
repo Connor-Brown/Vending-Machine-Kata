baa65b45bf302000ae1cfe11c83062a5b30bcd0e
Connor Brown <connormbrown1997@gmail.com>
2018-11-28 10:35:14 -0600

    Changed tests and code to not include a selected product

--- src/test/java/machineFeaturesTest/DisplayTest.java
+++ src/test/java/machineFeaturesTest/DisplayTest.java
@@ -41,11 +41,53 @@ public class DisplayTest {
 		assertEquals("$0.05", machine.getDisplay()); // after display gets checked again, display changes to INSERT COIN
 														// or current amount inserted
 	}
+	
+	@Test
+	public void checkDisplayAfterSelectingChipsWithNotEnoughMoneyInsertedShouldDisplayPriceOfChips()
+			throws InvalidProductException, NotAcceptedCoinException {
+		machine.insert(Coin.NICKEL);
+		machine.select("Chips");
+		assertEquals("PRICE $0.50", machine.getDisplay());
+		assertEquals("$0.05", machine.getDisplay()); // after display gets checked again, display changes to INSERT COIN
+														// or current amount inserted
+	}
+	
+	@Test
+	public void checkDisplayAfterSelectingCandyWithNotEnoughMoneyInsertedShouldDisplayPriceOfCandy()
+			throws InvalidProductException, NotAcceptedCoinException {
+		machine.insert(Coin.NICKEL);
+		machine.select("Candy");
+		assertEquals("PRICE $0.65", machine.getDisplay());
+		assertEquals("$0.05", machine.getDisplay()); // after display gets checked again, display changes to INSERT COIN
+														// or current amount inserted
+	}
 
 	@Test
 	public void checkDisplayAfterInsertingNickelShouldReturnFiveCentsInMachine() throws NotAcceptedCoinException {
 		machine.insert(Coin.NICKEL);
 		assertEquals("$0.05", machine.getDisplay());
+	}
+	
+	@Test
+	public void checkDisplayAfterInsertingDimeShouldReturn10CentsInMachine() throws NotAcceptedCoinException {
+		machine.insert(Coin.DIME);
+		assertEquals("$0.10", machine.getDisplay());
+	}
+	
+	@Test
+	public void checkDisplayAfterInsertingQuarterShouldReturnTwentyFiveCentsInMachine() throws NotAcceptedCoinException {
+		machine.insert(Coin.QUARTER);
+		assertEquals("$0.25", machine.getDisplay());
+	}
+	
+	@Test
+	public void checkDisplayAfterInserting75CentsShouldReturn75CentsInMachine() throws NotAcceptedCoinException {
+		machine.insert(Coin.NICKEL);
+		machine.insert(Coin.QUARTER);
+		machine.insert(Coin.DIME);
+		machine.insert(Coin.DIME);
+		machine.insert(Coin.QUARTER);
+		assertEquals("$0.75", machine.getDisplay());
 	}
 
 }
